a1d490b35193c0b7a1b94744cf520830983eda8c
李攸宁 <ovo5494@gmail.com>
2020-07-15 20:11:33 +0800

    [新增]
    新增商品详情sku显示

--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -1,43 +1,103 @@
-// pages/goods_detail/goods_detail.js
-import { request } from
-  "../../request/request.js"
+// pages/productdetail/productdetail.js
+import { request } from '../../request/request.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    goodsdetail:[]
+
+    product: {},
+    sku_show: false,
+    skus: [],
+    proSkus: [],
+    skuDisInfo: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const ID = options.ID;
-    console.log(ID);
-    this.getGoodsDetail(ID)
-    
+
+    const { ID } = options
+    console.log(ID)
+    this.getProductInfo(ID)
+
 
   },
-  async getGoodsDetail(ID){
-    const res = await request({url:"/Product/GetProduct",data:{ID}});
+
+  async getProductInfo(product_id) {
+
+    const res = await request({
+      url: "/Product/GetProduct",
+      data: { id: product_id },
+      method: "get"
+    })
     console.log(res)
+    res.Product.ProductMainImg = JSON.parse(res.Product.ProductMainImg)
     res.Product.ProductSlideImgs = JSON.parse(res.Product.ProductSlideImgs)
     res.Product.ProductDetailImg = JSON.parse(res.Product.ProductDetailImg)
-    this.setData({
-      goodsdetail:res.Product
+    res.Product.ProductSkuValues = JSON.parse(res.Product.ProductSkuValues)
+    let skuDisInfo = "请选择 "
+    res.Product.ProductSkuValues.forEach(v => {
+
+      v.selectedValue = null
+      skuDisInfo += v.name + " "
 
     })
+
+    // res.skus.forEach(v=>{
+
+    //   v.selectedValue=null
+    //   skuDisInfo+=v.attrName+" "
+
+    // })
+
+    this.setData({
+      product: res.Product,
+      skus: res.Skus,
+      proSkus: res.ProductSkus,
+      skuDisInfo: skuDisInfo
+    })
+
   },
-  handlePrevewImage:function(e){
-    
-    const urls= this.data.goodsdetail.ProductSlideImgs;
-    const current = e.currentTarget.dataset.url;
-    wx.previewImage({
-      current: current,
-      urls: urls,
-    });
+  showSku() {
+
+    this.setData({ sku_show: true })
+  },
+  onClose() {
+    this.setData({ sku_show: false });
+    let skuDisInfo = ""
+    let { product } = this.data
+    product.productSkuValues.forEach(v => {
+
+      if (v.selectedValue != null) {
+
+        skuDisInfo += v.selectedValue + " "
+
+      }
+
+
+    })
+    if (skuDisInfo != "") {
+
+      this.setData({ skuDisInfo: "已选择 " + skuDisInfo })
+    }
+
+  },
+  handleSkuChange(event) {
+    console.log(event)
+    const { attrname, attrvalue } = event.currentTarget.dataset
+    let { product } = this.data
+    product.ProductSkuValues.forEach(v => {
+
+      if (v.name == attrname) {
+        v.selectedValue = attrvalue
+      }
+    })
+    this.setData({ product })
+
+
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
